5cbcfffd7bc4d75f26655b3ae9e157819220120f
Gregory <kamapcuc@gmail.com>
2016-04-13 20:21:11 +0400

    ATGSM-5976

--- morph/src/main/java/org/apache/lucene/morphology/analyzer/WordFormFilter.java
+++ morph/src/main/java/org/apache/lucene/morphology/analyzer/WordFormFilter.java
@@ -3,19 +3,19 @@ package org.apache.lucene.morphology.analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.util.FilteringTokenFilter;
-import org.apache.lucene.morphology.Morphology;
+import org.apache.lucene.morphology.LuceneMorphology;
 import org.apache.lucene.util.Version;
 
 import java.io.IOException;
 
 public class WordFormFilter extends FilteringTokenFilter {
 
-    private final Morphology morphology;
+    private final LuceneMorphology morphology;
     private final String[] grammemas;
 
     private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
 
-    public WordFormFilter(TokenStream tokenStream, Morphology morphology, String[] grammemas) {
+    public WordFormFilter(TokenStream tokenStream, LuceneMorphology morphology, String[] grammemas) {
         super(Version.LUCENE_46, tokenStream);
         this.morphology = morphology;
         this.grammemas = grammemas;
@@ -23,6 +23,8 @@ public class WordFormFilter extends FilteringTokenFilter {
 
     @Override
     protected boolean accept() throws IOException {
-        return morphology.testWordForm(termAtt.toString(), grammemas);
+        String term = termAtt.toString();
+        return morphology.checkString(term) && morphology.testWordForm(term, grammemas);
     }
+
 }
